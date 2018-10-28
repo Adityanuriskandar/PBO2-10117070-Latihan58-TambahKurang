2a8b403a0a4f1e1ea8aa5578adb6406f856a239e
Aditya Nur Iskandar <43404691+Adityanuriskandar@users.noreply.github.com>
2018-10-28 21:42:19 +0700

    Add files via upload

---
+++ PBO2-10117070-Latihan58-TambahKurang/src/pbo2/pkg10117070/latihan58/tambahkurang/PBO210117070Latihan58TambahKurang.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117070.latihan58.tambahkurang;
+
+/**
+ *
+ * @author Aditya
+ * NAMA                 : Aditya Nur Iskandar
+ * KELAS                : PBO2
+ * NIM                  : 10117070
+ * Deskripsi Program    : Program ini berisi program untuk menghitung
+ * penjumlahan dan selisih dari 2 angka
+ */
+public class PBO210117070Latihan58TambahKurang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Bilangan b = new Bilangan();
+        JumlahBilangan j = new JumlahBilangan();
+        SelisihBilangan s = new SelisihBilangan();
+        
+        j.tampilHasilJumlah();
+        s.tampilSelisih();
+    }
+    
+}
